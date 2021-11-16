f337da96fc19953b54c7007981c1cc417ba1119e
krutoi-muzhik <71013428+krutoi-muzhik@users.noreply.github.com>
2021-11-16 05:12:57 +0300

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -21,52 +21,83 @@ Assembling (const char *input_path, const char *object_path) {
 	int value = 0;
 	char reg = 0;
 	int rip = 0;
+	mark_t *mark_tab = (mark_t *) calloc (MARK_TAB_SIZE, sizeof (mark_t));
+	int tmp;
+	int mark_num = 0;
 	int count = 0;
 
 	while (fscanf (input_file, "%s", command) != EOF) {
 		count = 0;
 		// printf ("%s   ", command);
-		#define DEF_CMD(name, cmd_num, t, arg, performance)					\
-			if (strcmp (command, #name) == 0) {								\
-				if (arg) {													\
-					fscanf (input_file, "%s", arg_s);						\
-					sscanf (arg_s, " [%d]%n", &value, &count);				\
-					if (count) {											\
-						code[rip++] = cmd_num | (RAM_NUM_M << MODE_SET);	\
-						code[rip++] = value;								\
-						continue;											\
-					}														\
-					sscanf (arg_s, "%d%n", &value, &count);					\
-					if (count) {											\
-						code[rip++] = cmd_num | (NUM_M << MODE_SET);		\
-						code[rip++] = value;								\
-						continue;											\
-					}														\
-					sscanf (arg_s, " [%cx]%n", &reg, &count);				\
-					if (count) {											\
-						value = (int) reg - 96;								\
-						code[rip++] = cmd_num | (RAM_REG_M << MODE_SET);	\
-						code[rip++] = value;								\
-						continue;											\
-					}														\
-					sscanf (arg_s, "%cx%n", &reg, &count);					\
-					if (count) {											\
-						value = (int) reg - 96;								\
-						code[rip++] = cmd_num | (REG_M << MODE_SET);		\
-						code[rip++] = value;								\
-						continue;											\
-					}														\
-					code[rip++] = cmd_num | (NUM_M << MODE_SET);			\
-					printf ("default\n");									\
-				} else {													\
-					code[rip++] = cmd_num;									\
-				}															\
-			}
+		#define DEF_CMD(name, cmd_num, t, arg, performance)	{					\
+			if (strcmp (command, #name) == 0) {									\
+				if (t) {														\
+					code[rip++] = cmd_num;										\
+					fscanf (input_file, "%s", arg_s);							\
+					if (tmp = MarkSeek (mark_tab, &mark_num, arg_s, rip)) {		\
+						code[rip++] = tmp;										\
+						continue;												\
+					} else {													\
+						code[rip++] = -1;										\
+						continue;												\
+					}															\
+				}																\
+				if (arg) {														\
+					fscanf (input_file, "%s", arg_s);							\
+					sscanf (arg_s, " [%d]%n", &value, &count);					\
+					if (count) {												\
+						code[rip++] = cmd_num | (RAM_NUM_M << MODE_SET);		\
+						code[rip++] = value;									\
+						continue;												\
+					}															\
+					sscanf (arg_s, "%d%n", &value, &count);						\
+					if (count) {												\
+						code[rip++] = cmd_num | (NUM_M << MODE_SET);			\
+						code[rip++] = value;									\
+						continue;												\
+					}															\
+					sscanf (arg_s, " [%cx]%n", &reg, &count);					\
+					if (count) {												\
+						value = (int) reg - 96;									\
+						code[rip++] = cmd_num | (RAM_REG_M << MODE_SET);		\
+						code[rip++] = value;									\
+						continue;												\
+					}															\
+					sscanf (arg_s, "%cx%n", &reg, &count);						\
+					if (count) {												\
+						value = (int) reg - 96;									\
+						code[rip++] = cmd_num | (REG_M << MODE_SET);			\
+						code[rip++] = value;									\
+						continue;												\
+					}															\
+					code[rip++] = cmd_num | (NUM_M << MODE_SET);				\
+					printf ("default\n");										\
+				} else {														\
+					code[rip++] = cmd_num;										\
+				}																\
+			} else {															\
+				if (command[strlen (command) - 1] == ':') {						\
+					command[strlen (command) - 1] = '\0';						\
+					mark_tab[mark_num].rip = rip;								\
+					strcpy (mark_tab[mark_num].mark_name, command);				\
+					mark_num++;													\
+				}																\
+			}																	\
+		}
 
 		#include "command.h"
 		#undef DEF_CMD
 	}
+
 	fclose (input_file);
+
+
+	for (int i = 0; i < mark_num; i++) {
+		for (int j = 0; j < mark_tab[i].use_count; j++) {
+			printf ("%s  %d\n", mark_tab[i].mark_name, mark_tab[i].rip);
+			code[mark_tab[i].mark_usage[j]] = mark_tab[i].rip;
+		}
+	}
 
 	FILE *object_file;
 	if ((object_file = fopen (object_path, "wb")) == NULL)
@@ -76,13 +107,25 @@ Assembling (const char *input_path, const char *object_path) {
 
 	fclose (object_file);
 
-	// for (int i = 0; i < rip; i++) {
-	// 	// printf ("code[%d] = %d\n", i, code[i]);
-	// 	fprintf (object_file, "%d ", code[i]);
-	// }
+	for (int i = 0; i < rip; i++) {
+		printf ("code[%d] = %d\n", i, code[i]);
+	}
 }
 
-void
-TextClean (char *text) {
+int
+MarkSeek (mark_t *mark_tab, int *tab_size, const char *name, int rip) {
+	int i = 0;
+	while (strcmp (mark_tab[i].mark_name, name) != 0) {
+		if (i == *tab_size) {
+			*tab_size++;
+			strcpy (mark_tab[*tab_size].mark_name, name);
+			mark_tab[*tab_size].rip = -1;
+			mark_tab[*tab_size].mark_usage[0] = rip;
+			mark_tab[*tab_size].use_count = 1;
+			return 0;
+		}
+		i++;
+	}
 
+	return mark_tab[i].rip;
 }
